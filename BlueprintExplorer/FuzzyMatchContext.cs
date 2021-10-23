54d63b26ee81d956132b47f86cf4ea6f5b36682c
factubsio <65080026+factubsio@users.noreply.github.com>
2021-10-23 13:58:46 +0100

    comment stuff?

--- BlueprintExplorer/FuzzyMatchContext.cs
+++ BlueprintExplorer/FuzzyMatchContext.cs
@@ -52,16 +52,21 @@ namespace BlueprintExplorer
     {
         private static FuzzyMatchResult<T> FuzzyMatch(FuzzyMatchResult<T> result)
         {
+            //eye indexes needle, thread indexes haystack.
             int eye = 0;
             int thread = -1;
 
             var needle = result.Context.Needle;
             var haystack = result.Haystack;
 
+            //find a common prefix if any, so n:cat h:catsgrace is better than n:cat h:blahcatsgrace
             thread = haystack.IndexOf(needle[eye]);
             if (thread == 0)
                 result.Bonus = 2.0f;
 
+            //penalise matches that don't have a common prefix, while increasing eye(needle) and thread(haystack) to the first match, so:
+            //n:bOb  h:hellOworldbob
+            //   ^         ^
             while (thread == -1 && eye < needle.Length)
             {
                 if (eye == 0)
@@ -72,27 +77,37 @@ namespace BlueprintExplorer
                 eye++;
             }
 
+            //Continue to match the next eye(needle) greedily to haystack
             while (eye < needle.Length)
             {
+
+                //find the next point in haystack that matches our eye:
+                //n:bOb h:helloworldBob
+                //    ^             ^
                 thread = haystack.IndexOf(needle[eye], thread);
                 if (thread == -1)
                     break;
+
+                //continue matching while both are in sync
                 MatchSpan span = new MatchSpan(thread);
                 while (thread < haystack.Length && eye < needle.Length && needle[eye] == haystack[thread])
                 {
+                    //if this span is rooted at the start of the word give a bonus because start is most importatn
                     if (span.From == 0 && eye > 0)
                         result.Bonus += result.Bonus;
                     eye++;
                     thread++;
                 }
 
+                //record the end of the span
                 span.End(thread);
                 result.matches.Add(span);
+
+                //update some stats that get used for scoring
                 if (span.Length > result.BestRun)
                     result.BestRun = span.Length;
                 if (span.Length == 1)
                     result.SingleRuns++;
-
                 result.TotalMatched += span.Length;
             }
             result.HaystackMatched = result.TotalMatched / (float)haystack.Length;
