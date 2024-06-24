3ddbec78dd4744448297e58e5db41f2adbd4eef7
xADDBx <62178123+xADDBx@users.noreply.github.com>
2024-06-24 22:23:07 +0200

    Forgor

--- BlueprintExplorer/SplashScreenChooserJobbie.cs
+++ BlueprintExplorer/SplashScreenChooserJobbie.cs
@@ -241,7 +241,7 @@ namespace BlueprintExplorer
                 string filename = BlueprintDB.FileNameFor(toLoad.Version.Version, toLoad.Version.Game);
                 Uri latestVersionUrl = null;
                 if (toLoad.Version.Game == "Wrath") latestVersionUrl = new Uri($"{host}/{toLoad.Version.Version}/{filename}");
-                else latestVersionUrl = new Uri($"{host}/RTv{toLoad.Version.Version}/{filename}");
+                else latestVersionUrl = new Uri($"{host}/{toLoad.Version.Version}_RT/{filename}");
                 var client = new WebClient();
 
                 string tmp = Path.Combine(CacheDir, "binz_download.tmp");
